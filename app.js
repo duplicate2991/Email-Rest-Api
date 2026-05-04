2efbfb104605d4664ba56b1030ce13048db12c8a
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2026-05-04 08:53:27 +0530

    Initialize Express app with middleware and routes

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const routes = require('./routes/v1');
+const errorHandler = require('./middlewares/error.middleware');
+const rateLimiter = require('./middlewares/rateLimit.middleware');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(rateLimiter);
+
+app.use('/api/v1', routes);
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: 'Route not found' });
+});
+
+// error handler
+app.use(errorHandler);
+
+module.exports = app;
