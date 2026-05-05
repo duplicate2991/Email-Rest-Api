2f2c8856a8fe48344ffcd7c4d3027b592fe952e9
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2026-05-05 17:21:20 +0530

    Update i.index.js

--- i.index.js
+++ i.index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = 25;
 
 app.use(cors());
 app.use(bodyParser.json());
